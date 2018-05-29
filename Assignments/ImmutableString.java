fdfc4073eb029379df8e73dc060f0a059bac25a7
Nikhil <nikhil.bhole.111@gmail.com>
2018-05-29 15:05:58 +0530

    immutabl;e String demo

---
+++ Assignments/ImmutableString.java
@@ -0,0 +1,24 @@
+package retraining;
+
+/**
+ * date: 29-05-2018
+ * this is java program for immutable String demo
+ * @author Nikhil
+ *
+ */
+
+public class ImmutableString {
+
+	public static void main(String[] args) {
+
+		String s1 = "Hello";
+		String s2 = "Friends";
+		
+		System.out.println("String s1 : "+s1+"   hashcode : "+s1.hashCode());
+		
+		s1 = s1+s2;
+		
+		System.out.println("String s1 : "+s1+"   hashcode : "+s1.hashCode());
+	}
+
+}
