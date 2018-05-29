262c8c831ded543123fd647e6d2b94372625b9e2
NikhilBhole <38826003+NikhilBhole@users.noreply.github.com>
2018-05-29 13:19:53 +0530

    Add files via upload

---
+++ Assignments/Hello.java
@@ -0,0 +1,16 @@
+package retraining;
+/**
+ * date: 29-05-2018
+ * this is first java program - how to compile and run
+ * @author Nikhil
+ *
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("welcome to EZEON");
+
+	}
+
+}
